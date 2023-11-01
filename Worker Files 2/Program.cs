7dea89335bedd331375b83880fa9d281f7e5bc15
lonys <lonys@ONYSZEK>
2023-11-01 18:34:02 +0100

    Add project files.

---
+++ Worker Files 2/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Workers Files");
+        Console.WriteLine("Please enter the number");
+        Console.WriteLine("1.Name");
+        Console.WriteLine("2.Surname");
+        Console.WriteLine("3.Pesel");
+        Console.WriteLine("4.Phone number");
+
+        string choice = Console.ReadLine();
+
+        Console.WriteLine($"You have chosen nr: {choice}");
+
+        string Name = "Łukasz";
+        string Surname = "Onyszko";
+        string Pesel = "8284821818";
+        string PhoneNumber = "1234567890";
+
+        Console.WriteLine($"Name: {Name}");
+        Console.WriteLine($"Surname {Surname}");
+        Console.WriteLine($"Pesel: {Pesel}");
+        Console.WriteLine($"Phone number: {PhoneNumber}");
+    }
+}
